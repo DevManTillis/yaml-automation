6735e68e88a59e329f77536b14a241ac11862c79
DevManTillis <migueltillisjr@gmail.com>
2020-08-31 18:25:40 -0700

    latest commit

--- mssql_server/DB_SCHEMA.py
+++ mssql_server/DB_SCHEMA.py
@@ -1,14 +1,14 @@
 SCHEMA=r'''
 CREATE TABLE Project
 (
-  project_id CHAR(36) NOT NULL,
-  name VARCHAR(25) NOT NULL,
+  project_id CHAR(36) NOT NULL UNIQUE,
+  name VARCHAR(25) NOT NULL UNIQUE,
   PRIMARY KEY (project_id)
 );
 
 CREATE TABLE AnsibleConfiguration
 (
-  config_id CHAR(36) NOT NULL,
+  config_id CHAR(36) NOT NULL UNIQUE,
   json_config VARCHAR(1000) NOT NULL,
   project_id CHAR(36) NOT NULL,
   PRIMARY KEY (config_id),
@@ -17,7 +17,7 @@ CREATE TABLE AnsibleConfiguration
 
 CREATE TABLE Command
 (
-  cmd_id CHAR(36) NOT NULL,
+  cmd_id CHAR(36) NOT NULL UNIQUE,
   arguments VARCHAR(36) NOT NULL,
   project_id CHAR(36) NOT NULL,
   PRIMARY KEY (cmd_id),
@@ -26,7 +26,7 @@ CREATE TABLE Command
 
 CREATE TABLE Playbook
 (
-  playbook_id CHAR(36) NOT NULL,
+  playbook_id CHAR(36) NOT NULL UNIQUE,
   playbook_name VARCHAR(25) NOT NULL,
   playbook_url VARCHAR(200) NOT NULL,
   project_id CHAR(36) NOT NULL,
@@ -36,8 +36,8 @@ CREATE TABLE Playbook
 
 CREATE TABLE Host
 (
-  host_id CHAR(36) NOT NULL,
-  host_name VARCHAR(25) NOT NULL,
+  host_id CHAR(36) NOT NULL UNIQUE,
+  host_name VARCHAR(25) NOT NULL UNIQUE,
   project_id CHAR(36) NOT NULL,
   PRIMARY KEY (host_id),
   FOREIGN KEY (project_id) REFERENCES Project(project_id) ON DELETE CASCADE
@@ -45,9 +45,9 @@ CREATE TABLE Host
 
 CREATE TABLE Role
 (
-  role_id CHAR(36) NOT NULL,
+  role_id CHAR(36) NOT NULL UNIQUE,
   role_url VARCHAR(200) NOT NULL,
-  role_name VARCHAR(25) NOT NULL,
+  role_name VARCHAR(25) NOT NULL UNIQUE,
   project_id CHAR(36) NOT NULL,
   PRIMARY KEY (role_id),
   FOREIGN KEY (project_id) REFERENCES Project(project_id) ON DELETE CASCADE
@@ -55,7 +55,7 @@ CREATE TABLE Role
 
 CREATE TABLE Output
 (
-  output_id CHAR(36) NOT NULL,
+  output_id CHAR(36) NOT NULL UNIQUE,
   json_output VARCHAR(8000) NOT NULL,
   start_time VARCHAR(25) NOT NULL,
   end_time VARCHAR(25) NOT NULL,
