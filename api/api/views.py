4ddea3106838b3568dcd87b3bdea359108944e65
DevManTillis <migueltillisjr@gmail.com>
2020-09-10 03:46:02 -0700

    latest commit

--- api/api/views.py
+++ api/api/views.py
@@ -1,4 +1,5 @@
 from rest_framework import viewsets
+from rest_framework import filters
 from . import serializers
 from . import models 
 
@@ -6,33 +7,47 @@ from . import models
 class ProjectViewset(viewsets.ModelViewSet):
     serializer_class = serializers.ProjectSerializer
     queryset = models.Project.objects.all()
+    filter_backends = [filters.SearchFilter]
+    search_fields = ['id']
 
 
 class CommandViewset(viewsets.ModelViewSet):
     serializer_class = serializers.CommandSerializer
     queryset = models.Command.objects.all()
+    filter_backends = [filters.SearchFilter]
+    search_fields = ['project_id__id']
 
 
 class PlaybookViewset(viewsets.ModelViewSet):
     serializer_class = serializers.PlaybookSerializer
     queryset = models.Playbook.objects.all()
+    filter_backends = [filters.SearchFilter]
+    search_fields = ['project_id__id']
 
 
 class AnsibleConfViewset(viewsets.ModelViewSet):
     serializer_class = serializers.AnsibleConfigurationSerializer
     queryset = models.AnsibleConfiguration.objects.all()
+    filter_backends = [filters.SearchFilter]
+    search_fields = ['project_id__id']
 
 
 class HostViewset(viewsets.ModelViewSet):
     serializer_class = serializers.HostSerializer
     queryset = models.Host.objects.all()
+    filter_backends = [filters.SearchFilter]
+    search_fields = ['project_id__id']
 
 
 class RoleViewset(viewsets.ModelViewSet):
     serializer_class = serializers.RoleSerializer
     queryset = models.Role.objects.all()
+    filter_backends = [filters.SearchFilter]
+    search_fields = ['project_id__id']
 
 
 class OutputViewset(viewsets.ModelViewSet):
     serializer_class = serializers.OutputSerializer
     queryset = models.Output.objects.all()
+    filter_backends = [filters.SearchFilter]
+    search_fields = ['project_id__id']
--- api/migueltillis/wsgi.py
+++ api/migueltillis/wsgi.py
@@ -1,5 +1,5 @@
 """
-WSGI config for yautomation project.
+WSGI config for migueltillis project.
 
 It exposes the WSGI callable as a module-level variable named ``application``.
 
@@ -11,6 +11,6 @@ import os
 
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'yautomation.settings')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'migueltillis.settings')
 
 application = get_wsgi_application()
